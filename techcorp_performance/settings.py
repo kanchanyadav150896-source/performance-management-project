a3c71c6917ad797de1b6c9ffe2791f6641ac14ba
kanchanyadav <kanchanyadav150896@gmail.com>
2025-09-29 07:19:14 +0530

    first commit- Performance management project

--- techcorp_performance/settings.py
+++ techcorp_performance/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles', 'users','reviews', 'goals', 'rest_framework', 
+    'drf_yasg',   
 ]
 
 MIDDLEWARE = [
